3e50aa0fcb63cd4f100d8799c6fafc823ae7ce67
Joe Kaufeld <joe.kaufeld@gmail.com>
2020-01-12 17:31:18 -0500

    rip out preact
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>

--- filamentcolors/preact_front_end/src/components/loading_gif/index.js
+++
@@ -1,8 +0,0 @@
-import style from './style.scss';
-import gif from './loading.gif';
-
-export default function LoadingGif() {
-    return (
-        <img src={gif} alt='loading spinner' className={style.centerIt}/>
-    )
-}
